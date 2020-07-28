594c145590ee7b38afb6cc31dcc9fabe0d7592b3
徐款 <kuan.xu@hand-china.com>
2020-07-28 18:39:53 +0800

    add: 静态资源

---
+++ precache-manifest.8313cc2295e0c90d7e76e2b19b76dd2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10abeb36fb3ad3fa1698e380e7a1ed2",
+    "url": "/summarytableexport/index.html"
+  },
+  {
+    "revision": "49309639a99074d949a1",
+    "url": "/summarytableexport/static/css/2.e7180fea.chunk.css"
+  },
+  {
+    "revision": "340b06de10cf1ea4ea87",
+    "url": "/summarytableexport/static/css/main.21eee12d.chunk.css"
+  },
+  {
+    "revision": "49309639a99074d949a1",
+    "url": "/summarytableexport/static/js/2.45f9e516.chunk.js"
+  },
+  {
+    "revision": "b3e8ebebd2bed1b75d26cc95d0b56640",
+    "url": "/summarytableexport/static/js/2.45f9e516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "340b06de10cf1ea4ea87",
+    "url": "/summarytableexport/static/js/main.d3854c72.chunk.js"
+  },
+  {
+    "revision": "e9c827a279f46260541b",
+    "url": "/summarytableexport/static/js/runtime-main.22555208.js"
+  }
+]);
