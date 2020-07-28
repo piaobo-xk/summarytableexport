ad34a6766e0c3d7e75d3eb0596b1eda5558a7412
徐款 <kuan.xu@hand-china.com>
2020-07-28 16:56:12 +0800

    init

---
+++ precache-manifest.7afee0385966866ea0f8cf38f326d34c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1c412f8b962ecabfed3d160cef5a79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b6cd0a07acad119bbd9",
+    "url": "/static/css/2.e7180fea.chunk.css"
+  },
+  {
+    "revision": "797d72941f9685b35016",
+    "url": "/static/css/main.21eee12d.chunk.css"
+  },
+  {
+    "revision": "1b6cd0a07acad119bbd9",
+    "url": "/static/js/2.c65b8084.chunk.js"
+  },
+  {
+    "revision": "b3e8ebebd2bed1b75d26cc95d0b56640",
+    "url": "/static/js/2.c65b8084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797d72941f9685b35016",
+    "url": "/static/js/main.2298a978.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  }
+]);
